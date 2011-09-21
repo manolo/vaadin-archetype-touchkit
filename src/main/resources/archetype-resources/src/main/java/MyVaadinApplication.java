a97fe28b153584d1c32cf0cf9cf896a666f0009b
matti.tahvonen@itmill.com <matti.tahvonen@itmill.com@de684a11-831f-0410-ab27-cb86445eb682>
2011-09-21 11:31:38 +0000

    almost working touchkit archetype project
    git-svn-id: http://dev.vaadin.com/svn/integration/maven/archetypes/vaadin-archetype-touchkit@21210 de684a11-831f-0410-ab27-cb86445eb682

---
+++ src/main/resources/archetype-resources/src/main/java/MyVaadinApplication.java
@@ -0,0 +1,31 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import com.vaadin.addon.touchkit.ui.TouchKitApplication;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Window;
+
+/**
+ * The Application's "main" class
+ */
+@SuppressWarnings("serial")
+public class MyVaadinApplication extends TouchKitApplication
+{
+    
+    @Override
+    public void onBrowserDetailsReady() {
+        Button button = new Button("Click Me");
+        button.addListener(new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                window.addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        getMainWindow.addComponent(button);
+    }
+
+    
+}
