02ef254e6c0aed0bf6a42a59725b56cd762f44d2
matti.tahvonen@itmill.com <matti.tahvonen@itmill.com@de684a11-831f-0410-ab27-cb86445eb682>
2013-01-04 10:15:03 +0000

    work towards TouchKit 3 compatible archetype, now servlet 3 based
    git-svn-id: http://dev.vaadin.com/svn/integration/maven/archetypes/vaadin-archetype-touchkit@25257 de684a11-831f-0410-ab27-cb86445eb682

--- src/main/resources/archetype-resources/src/main/java/MyTouchKitUI.java
+++ src/main/resources/archetype-resources/src/main/java/MyTouchKitUI.java
@@ -3,31 +3,33 @@
 #set( $symbol_escape = '\' )
 package ${package};
 
-import com.vaadin.addon.touchkit.ui.TouchKitUI;
+
+import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
 
 /**
  * The UI's "main" class
  */
-@SuppressWarnings("serial")
-public class MyVaadinUI extends TouchKitUI
-{
-	@Override
+@Widgetset("com.vaadin.demo.diipaaipa.gwt.AppWidgetSet")
+public class MyTouchKitUI extends UI {
+    @Override
     protected void init(VaadinRequest request) {
-		setContent(new CssLayout());
+        setContent(new CssLayout());
+        final CssLayout cssLayout = new CssLayout();
         Button button = new Button("Click Me");
         button.addClickListener(new ClickListener() {
             public void buttonClick(ClickEvent event) {
-            	event.getButton().getUI().addComponent(new Label("Thank you for clicking"));
+                cssLayout.addComponent(new Label("Thank you for clicking"));
             }
         });
-        addComponent(button);
+        cssLayout.addComponent(button);
+        setContent(cssLayout);
     }
 
-    
 }
